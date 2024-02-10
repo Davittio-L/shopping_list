494b03e43b6f67da3505a328f1d25e0b694f4531
Colten <129473270+Davittio-L@users.noreply.github.com>
2024-02-09 22:56:36 -0500

    Created new list logic.
    newList logic created - not working --yet.

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ var ul = document.getElementsByTagName('ul')[0];
 function addItemLength() {
     return document.getElementById('userInput').ariaValueMax.length;
 }
+
+// Check how many list elements exist
+function checkList() {
+    return document.getElementsByTagName('li').length;
+}
+
+// Create new list items
+function createListItem() {
+    var li = document.createElement('li');
+    li.classList.add('listItem');
+    li.append(document.createTextNode(input.value));
+    li.addEventListener('click', toggleDone);
+    ul.appendChild(li);
+    input.value = '';
+}
