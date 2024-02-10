7da184f4a9c41a1ec6e143a5a9eb5dfa48643944
Colten <129473270+Davittio-L@users.noreply.github.com>
2024-02-10 08:44:04 -0500

    Javascript logic for lists done
    Compeleted hiding empty lists, toggling through and deleteing items.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function createListItem() {
     li.addEventListener('click', toggleDone);
     ul.appendChild(li);
     input.value = '';
-}
+
 
 // Create delete button - style it and append to li
 var deleteButton = document.createElement('button');
@@ -30,3 +30,33 @@ deleteButton.classList.add('custBtn');
 deleteButton.appendChild(delX);
 deleteButton.addEventListener('click', delItem);
 li.append(deleteButton);
+
+// Hide Empty List text
+if(checkList() > 0) {
+    var emptyList = document.querySelector('h2');
+    emptyList.style.display = "none";
+}
+
+// Toggle line-through on click
+function toggleDone() {
+    li.classList.toggle('done');
+}
+
+// Delete Items
+function deleteItem() {
+    li.remove();
+    if(checkList() == 0) {
+        var emptyList = document.querySelector('h2');
+        emptyList.style.display = "block";
+    }
+}
+}
+
+//on keypress enter
+function addKey(event) {
+    if(addItemLength() > 0 && event.keyCode == 13) {
+        createListItem();
+    }
+} 
+addBtn.addEventListener('click', addClick);
+input.addEventListener('keypress', addKey);
